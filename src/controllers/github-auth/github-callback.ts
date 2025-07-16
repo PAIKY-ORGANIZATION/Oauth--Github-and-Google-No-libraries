3d2ab5a44a9f204c479e12813e44eb522c5fe2ce
Priapisman677 <expeliamm02@hotmail.com>
2025-07-16 10:23:20 -0700

    update

--- src/controllers/github-auth/github-callback.ts
+++ src/controllers/github-auth/github-callback.ts
@@ -1,10 +1,32 @@
 import { Request, Response } from 'express';
-
+import axios from 'axios';
 
 export const githubCallback = async (req: Request,res: Response) => {
 
-	const response: ServerResponse = { message: 'Success', success: true, data: {  } } 
+	const code = req.query.code //$ this code can only be used once.
 
-	res.send(response );
+	const response = await axios.post('https://github.com/login/oauth/access_token', {
+		client_id: process.env.GITHUB_CLIENT_ID,
+		client_secret: process.env.GITHUB_CLIENT_SECRET,
+		code
+	})
+
+	const {access_token} = response.data
+
+	const userRes = await axios.get('https://api.github.com/user', {
+		headers: {Authorization: 'Bearer ' + access_token}
+	})
+
+	console.log(userRes.data);
+	
+
+
+	const emailRes = await axios.get('https://api.github.com/user/emails', {
+		headers: {Authorization: 'Bearer ' + access_token}
+	})
+
+	console.log(emailRes.data);
+
+	res.send('Success');
 };
 
--- src/controllers/github-auth/github-redirect.ts
+++ src/controllers/github-auth/github-redirect.ts
@@ -4,4 +4,6 @@ export const githubRedirect = async(req: Request, res: Response)=>{
     const redirectUri = "http://localhost:3001/auth/github/callback" //$ It is kind of redundant since we already set this up in our GitHub application but it seems that it is some kind of security measure to have to verify it.
 
     const clientId = process.env.GITHUB_CLIENT_ID
+    const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user:email`
+    res.redirect(url)
 }
