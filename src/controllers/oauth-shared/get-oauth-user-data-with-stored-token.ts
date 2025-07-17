725cd785e0b135845a2ca95824aa69277edc2215
Priapisman677 <expeliamm02@hotmail.com>
2025-07-17 16:44:34 -0700

    update

--- src/controllers/oauth-shared/get-oauth-user-data-with-stored-token.ts
+++ src/controllers/oauth-shared/get-oauth-user-data-with-stored-token.ts
@@ -10,7 +10,7 @@ import { getGoogleAccessTokenByCode } from '../../lib/oauth/google/get-access-to
 //% This file focuses on using decryption to reuse the encrypted authorization token to REFETCH user data from GitHub.
 //% ➡️➡️➡️ It might be the case that the user information has updated in GitHub.
 
-
+//prettier-ignore
 export const getUserDataWithStoredTokenController = async(req: Request, res: Response)=>{
     const {userId} = (req as any).userId //$ Assuming that authorization middleware was successful.
 
@@ -24,8 +24,10 @@ export const getUserDataWithStoredTokenController = async(req: Request, res: Res
 
     const decryptedAccessToken = decrypt(user.encryptedOauthAccessToken, user.encryptedOauthAccessTokenIv!)
 
-
-    const getOauthUserDataByTokenFunction = oauthProviderName === 'github'?
+    //% If the stored OAuth provider is GitHub, use the GitHub "getGithubUserDataByToken" function. to retrieve the user data from GitHub using a GitHub access token. 
+    //% Vice versa for Google
+    const getOauthUserDataByTokenFunction =
+        oauthProviderName === 'github' ?
         getGithubUserDataByToken :
         getGoogleAccessTokenByCode
 
