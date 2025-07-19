1d39de99246b4132cddd977f35be96398643b6a7
Priapisman677 <expeliamm02@hotmail.com>
2025-07-19 09:44:20 -0700

    update

--- src/lib/oauth/google/get-access-token-with-code.ts
+++ src/lib/oauth/google/get-access-token-with-code.ts
@@ -1,8 +1,13 @@
 import axios  from 'axios'
+import { CALLBACK_PATH_GOOGLE } from '../../variables.js'
 
 export const getGoogleAccessTokenByCode = async(code: string)=>{
 
-    const redirectUri = "http://localhost:3001/api/auth/google/callback"
+        
+        const redirectUriBase = process.env.LOCAL_SERVER_BASE_URL //$ Looks like "http://localhost:3001"
+    
+        //! This same one must be used in the router. Router by def. starts with "/api"
+        const redirectUri = redirectUriBase + '/api' + CALLBACK_PATH_GOOGLE //$ Looks like "http://localhost:3001 + /api/auth/google/callback" 
 
     const tokenResponse = await axios.post(`https://oauth2.googleapis.com/token`, {
         code,
