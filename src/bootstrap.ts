c752004e11ef0cb2c028b4131ce5cc226e06c8af
Priapisman677 <expeliamm02@hotmail.com>
2025-07-19 08:03:00 -0700

    update

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,25 +1,31 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
+const setENV = () => {
+	//$ process.env.ENVIRONMENT is defined based on script ran from package.json
+	//$ Example: Running "npm run dev_local" will set process.env.ENVIRONMENT to "dev" which will load "config/dev.env"
+	dotenv.config({ path: `config/${process.env.ENVIRONMENT}.env` });
 
-const setENV = ()=>{
+	//$ These are shared between all environments, example: PORT
+	dotenv.config({ path: 'config/shared.env' });
 
-    //$ process.env.ENVIRONMENT is defined based on script ran from package.json
-    //$ Example: Running "npm run dev_local" will set process.env.ENVIRONMENT to "dev" which will load "config/dev.env"
-    dotenv.config({path: `config/${process.env.ENVIRONMENT}.env`}) 
-    
-    //$ These are shared between all environments, example: PORT
-    dotenv.config({path: 'config/shared.env'})
+	//* Check for required environment variables or throw stop execution.
+	const requiredVars = [
+		'CLIENT_ID_GITHUB',
+		'CLIENT_SECRET_GITHUB',
+		'CLIENT_ID_GOOGLE',
+		'CLIENT_SECRET_GOOGLE',
+        'LOCAL_SERVER_BASE_URL',
+		'ENCRYPTION_KEY',
+		'JWT_SECRET',
+	];
 
+	requiredVars.forEach((varName) => {
+		if (!process.env[varName]) {
+			throw new Error(
+				`Missing required environment variable: ${varName}`
+			);
+		}
+	});
+};
 
-     //* Check for required environment variables or throw stop execution.   
-    const requiredVars = ['PORT', 'CLIENT_ID_GITHUB', 'CLIENT_SECRET_GITHUB', 'CLIENT_ID_GOOGLE', 'CLIENT_SECRET_GOOGLE', 'ENCRYPTION_KEY', 'JWT_SECRET']
-
-    requiredVars.forEach((varName)=>{
-        if(!process.env[varName]){
-            throw new Error(`Missing required environment variable: ${varName}`)
-        }
-    })
-
-}
-
-setENV()
+setENV();
--- src/controllers/github-auth/redirect-to-github.ts
+++ src/controllers/github-auth/redirect-to-github.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from 'express';
 
 export const redirectToGithub = async(_req: Request, res: Response)=>{
-    const redirectUri = "http://localhost:3001/api/auth/github/callback" //$ It is kind of redundant since we already set this up in our GitHub application but it seems that it is some kind of security measure to have to verify it.
+
+    const redirectUriBase = process.env.LOCAL_SERVER_BASE_URL
+
+    const redirectUri = redirectUriBase+ "/api/auth/github/callback" //$ It is kind of redundant since we already set this up in our GitHub application but it seems that it is some kind of security measure to have to verify it.
 
     const clientId = process.env.CLIENT_ID_GITHUB
     const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user:email`
